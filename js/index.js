cc04bf766d28ff8014dff692de2efaa494f68bc8
Marina Neumann <marinaneumann2468@yahoo.com>
2014-09-26 12:10:08 -0700

    Establish bar graph representing data.

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var data = [4, 8, 15, 16, 23, 42];
+
+var x = d3.scale.linear()
+	.domain([0, d3.max(data)])
+	.range([0, 420]);
+
+
+d3.select(".bar").selectAll("div")
+    .data(data)
+    .enter()
+    .append("div")
+    .attr("class", "bar")
+		.style("width", function(d) { return x(d) + "px"; })
+    .text(function(d) { return d; });
+ 		 
